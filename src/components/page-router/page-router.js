6dccdb15fd8643d0b0e729577ff5bb0900504794
Torbjørn Angeltveit <torbjorn@angeltveit.info>
2019-03-28 19:07:49 +0100

    removed hashbang and added hack

--- src/components/page-router/page-router.js
+++ src/components/page-router/page-router.js
@@ -30,7 +30,7 @@ export default class PageRouter extends HTMLElement {
 
       })
     })
-    page({ hashbang: true })
+    page()
     const pathname = window.location.hash.slice(1)
     if(pathname) page(pathname)
   }
